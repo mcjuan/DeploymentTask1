9264974f94eb71ca5cbd9b6926d5281d632c47bd
Marcus Juan <mc.jn.001@gmail.com>
2025-08-22 17:55:46 +1000

    init

---
+++ DeploymentTask1/Form1.cs
@@ -0,0 +1,20 @@
+namespace DeploymentTask1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Hello! This app was packaged with WiX!");
+        }
+    }
+}
